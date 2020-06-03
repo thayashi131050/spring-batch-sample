c5b416af4f0ba29f5b47faac8abec314be210fa4
sprngTst001 <clqaq32618@yahoo.co.jp>
2020-06-03 14:43:56 +0900

    close #17
    mainプログラムでInputテーブルの初期化を行わないよう変更(事前に用意されている前提)
    
    testプログラムでInputテーブルの初期化・テストデータ作成を行うよう変更

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,18 +1,3 @@
-drop table if exists peopleInput;
-
-create table peopleInput( 
-  person_id SERIAL NOT NULL
-  , first_name VARCHAR (20)
-  , last_name VARCHAR (20)
-  , PRIMARY KEY (person_id)
-);
-
-insert into peopleInput (first_name, last_name) values ('abc','aaa');
-insert into peopleInput (first_name, last_name) values ('bcd','aaa');
-insert into peopleInput (first_name, last_name) values ('cde','aaa');
-insert into peopleInput (first_name, last_name) values ('def','aaa');
-
-
 drop table if exists peopleOutput;
 
 create table peopleOutput( 
